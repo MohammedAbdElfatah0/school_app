b28c912d21054efa0d50022b65036c655e894d43
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-03 01:22:29 +0200

    add black color

--- lib/core/constants/color_manager.dart
+++ lib/core/constants/color_manager.dart
@@ -4,5 +4,6 @@ class ColorManager {
   static const Color primaryColor = Color(0xFF3B71D8);
   static const Color secondaryColor = Color(0xFFFFD656);
   static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color blackColor = Color(0xFF000000);
   static const Color greyColor500 = Color.fromARGB(255, 158, 158, 158);
 }
