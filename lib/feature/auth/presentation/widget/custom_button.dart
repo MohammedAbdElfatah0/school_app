64cacd51e4040d86a4a0dca3f1871e3bc8cb1c9a
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-03 05:47:10 +0200

    edit UI firendly button

--- lib/feature/auth/presentation/widget/custom_button.dart
+++ lib/feature/auth/presentation/widget/custom_button.dart
@@ -3,21 +3,27 @@ import 'package:school_app/core/constants/color_manager.dart';
 import 'package:school_app/core/theme/text_style.dart';
 
 class CustomButton extends StatelessWidget {
-  const CustomButton({super.key, required this.title});
+  const CustomButton({super.key, required this.title, this.onPressed});
   final String title;
+  final VoidCallback? onPressed;
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      onPressed: () {},
-      style: ElevatedButton.styleFrom(
-        backgroundColor: ColorManager.primaryColor,
-        padding: EdgeInsets.symmetric(
-          horizontal: MediaQuery.sizeOf(context).width *0.37 ,
-          vertical: 14,
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          backgroundColor: ColorManager.primaryColor,
+          padding: EdgeInsets.symmetric(vertical: 14),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0),
+          ),
         ),
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12.0)),
+        child: Text(
+          title,
+          style: TextStyleApp.s18w600.copyWith(color: ColorManager.whiteColor),
+        ),
       ),
-      child: Text(title, style: TextStyleApp.s18w600.copyWith(color: ColorManager.whiteColor)),
     );
   }
 }
