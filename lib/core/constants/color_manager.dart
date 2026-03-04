11ade1be4c171e301a30d3f2d58f0a5ce3cfe71f
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-05 00:15:23 +0200

    add color error

--- lib/core/constants/color_manager.dart
+++ lib/core/constants/color_manager.dart
@@ -1,9 +1,12 @@
 import 'dart:ui';
 
+import 'package:flutter/widgets.dart';
+
 class ColorManager {
   static const Color primaryColor = Color(0xFF3B71D8);
   static const Color secondaryColor = Color(0xFFFFD656);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
   static const Color greyColor500 = Color.fromARGB(255, 158, 158, 158);
+  static const Color errorColor = Color(0xFFFF0000);
 }
