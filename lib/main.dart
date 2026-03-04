0bf630b6ba374c63afd6c4bd90dd35c38b63f632
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-05 01:21:05 +0200

    add file env to saving app

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:school_app/school_app.dart';
 
 import 'core/networks/supabase_service.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   SupabaseService.instance.init();
   runApp(const SchoolApp());
 }
