5e9a83c4b40149710428ad72f2950612aae41775
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-04 16:40:57 +0200

    - install supabase - init supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:school_app/school_app.dart';
 
+import 'core/networks/supabase_service.dart';
+
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SupabaseService.instance.init();
   runApp(const SchoolApp());
 }
