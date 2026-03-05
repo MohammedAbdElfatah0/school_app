6d35908bdb6a63fe58e10ef935bd5c1ffc683b6e
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-05 17:20:50 +0200

    Feat:header home screen

---
+++ lib/feature/home/presentation/widget/header_home.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:school_app/core/constants/color_manager.dart';
+import 'package:school_app/core/theme/text_style.dart';
+
+class HeaderHome extends StatelessWidget {
+  const HeaderHome({super.key, required this.name});
+  final String name;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(color: ColorManager.primaryColor),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        child: Column(
+          spacing: 5,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hi, $name',
+              style: TextStyleApp.s16w700.copyWith(
+                color: ColorManager.whiteColor,
+              ),
+            ),
+            Text(
+              'Welcome to School',
+              style: TextStyleApp.s18w600.copyWith(
+                color: ColorManager.whiteColor,
+              ),
+            ),
+            TextField(
+              style: const TextStyle(color: ColorManager.blackColor),
+              decoration: InputDecoration(
+                hintText: 'Search',
+                filled: true,
+                fillColor: ColorManager.whiteColor,
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                  borderSide: const BorderSide(
+                    color: ColorManager.whiteColor,
+                    width: 2,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: MediaQuery.of(context).size.height * 0.005),
+          ],
+        ),
+      ),
+    );
+  }
+}
