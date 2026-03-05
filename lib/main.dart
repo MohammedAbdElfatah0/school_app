a74ac84915fd80a6aa302be346f692d48c95b57d
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-05 13:29:28 +0200

    init supabase and env

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:school_app/core/config/env.dart';
 import 'package:school_app/school_app.dart';
-
-import 'core/networks/supabase_service.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await dotenv.load(fileName: ".env");
-  SupabaseService.instance.init();
+  await dotenv.load(fileName: "assets/env/.env");
+  await Supabase.initialize(url: Env.supabaseUrl, anonKey: Env.supabaseAnonKey);
   runApp(const SchoolApp());
 }
