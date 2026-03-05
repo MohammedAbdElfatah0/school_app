367c1a94fba6d1bdeda5a24d905e43db09d07cf7
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-05 14:36:31 +0200

    add color gray white

--- lib/core/constants/color_manager.dart
+++ lib/core/constants/color_manager.dart
@@ -7,6 +7,7 @@ class ColorManager {
   static const Color secondaryColor = Color(0xFFFFD656);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
-  static const Color greyColor500 = Color.fromARGB(255, 158, 158, 158);
+  static const Color greyColor500 = Color.fromARGB(255, 163, 161, 161);
+  static const Color greyColor = Color.fromARGB(255, 217, 214, 214);
   static const Color errorColor = Color(0xFFFF0000);
 }
