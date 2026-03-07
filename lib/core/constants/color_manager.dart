6033ba80190ca28c01ab031d21b19e9eb6494145
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-07 23:52:10 +0200

    add color green

--- lib/core/constants/color_manager.dart
+++ lib/core/constants/color_manager.dart
@@ -5,6 +5,7 @@ import 'package:flutter/widgets.dart';
 class ColorManager {
   static const Color primaryColor = Color(0xFF3B71D8);
   static const Color secondaryColor = Color(0xFFFFD656);
+  static const Color greenColor = Color(0xFF4CAF50);
   static const Color whiteColor = Color(0xFFFFFFFF);
   static const Color blackColor = Color(0xFF000000);
   static const Color greyColor500 = Color.fromARGB(255, 163, 161, 161);
