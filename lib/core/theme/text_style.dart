cdfd94c0f70fea660c2f8ab3fdbfa52d1e89f758
Mohammed Mohammed Abd El-Fatah <mohammedabdelfatah837@gmail.com>
2026-03-07 22:12:29 +0200

    add size 16 weight 600

--- lib/core/theme/text_style.dart
+++ lib/core/theme/text_style.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/rendering.dart';
+
 class TextStyleApp {
   // s 14
-  static const  TextStyle s14w400 = TextStyle(
+  static const TextStyle s14w400 = TextStyle(
     fontSize: 14,
     fontWeight: FontWeight.w400,
   );
@@ -21,6 +22,10 @@ class TextStyleApp {
   static const TextStyle s16w400 = TextStyle(
     fontSize: 16,
     fontWeight: FontWeight.w400,
+  );
+  static const TextStyle s16w600 = TextStyle(
+    fontSize: 16,
+    fontWeight: FontWeight.w600,
   );
   static const TextStyle s16w700 = TextStyle(
     fontSize: 16,
